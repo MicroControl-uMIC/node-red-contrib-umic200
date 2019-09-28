7ea050b31bc7d2acc9987b8394406a0b71b49306
Uwe Koppe <koppe@microcontrol.net>
2019-09-28 21:36:34 +0200

    Use class concept

--- umic200/umic-dio.js
+++ umic200/umic-dio.js
@@ -1,22 +1,90 @@
-/*jshint esversion: 6 */ 
+/*jshint esversion: 6 */
 
-const umic = require ('/home/umic/.node-red/node_modules/umic/build/Release/umic.node');
+//------------------------------------------------------------------------------------------------------
+// Required for version information of the module, this is only used in the first module
+//
+const path = require('path');
+const fs = require('fs');
+const pkg = require(path.join(__dirname, '..', 'package.json'));
 
-module.exports = function(RED) {
-    function UmicReadPin(config) {
-        RED.nodes.createNode(this,config);
+//------------------------------------------------------------------------------------------------------
+// Try to load the umic library
+//
+try
+{
+    const umic = require('/usr/local/lib/node_modules/umic');
+} 
+catch (error)
+{
+    console.log('oh no big error');
+    console.log(error);
+}
+
+
+/*
+ * module.exports = function(RED) {
+ *  //
+ * ----------------------------------------------------------------------------- //
+ * RED.log.info('node-red-contrib-umic version: ' + pkg.version);
+ * 
+ * function UmicReadPin(config) { RED.nodes.createNode(this, config);
+ * 
+ * var node = this; this.pin = config.pin; this.outputs = config.outputs;
+ * 
+ * node.on('input', function(msg) { let result =
+ * umic.dio_get_input_pin(parseInt(this.pin)); msg.payload = result;
+ * node.send(msg); });
+ *  } RED.nodes.registerType("umic-dio", UmicReadPin); };
+ */
+
+
+module.exports = function (RED) {
+    
+    //---------------------------------------------------------------------------------------------
+    // show version of package
+    //
+    RED.log.info('node-red-contrib-umic version: ' + pkg.version);
+    
+    //---------------------------------------------------------------------------------------------
+    // Definition of class 'UmicDioNode'
+    //
+    class UmicDioNode {
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+            this.pin=config.pin;
+            this.outputs=config.outputs;
+            
+            this.on('input', this.input);
+            this.on('close', this.destructor);
+            
+            setInterval(this.trigger, 1200);
+        }
         
-        var node = this;
-        this.pin=config.pin;
-        this.outputs=config.outputs;
+        destructor(done) {
+            done();
+            
+        }
         
-        node.on('input', function(msg) {
+        output(msg) {
             let result = umic.dio_get_input_pin(parseInt(this.pin));
             msg.payload=result;
-            node.send(msg);
-        });
+            return msg.payload;
+        }
         
+        input(msg) {
+            RED.log.info('bingo ');
+            let result = umic.info_get_system_temperature();
+            msg.payload=result;
+        }
+        
+        trigger()
+        {
+            // RED.log.info('bongo ');
+            // var newMsg = { payload: msg.payload.length }; --> this does not
+            // work
+        }
     }
-    RED.nodes.registerType("readPin",UmicReadPin);
-};
+    
+    RED.nodes.registerType('umic-dio', UmicDioNode);
+}
 
