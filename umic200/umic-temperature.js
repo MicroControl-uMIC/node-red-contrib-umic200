831a313eaf8751170b3b96e240ef420b4f682772
Uwe Koppe <koppe@microcontrol.net>
2019-09-28 21:35:03 +0200

    Improve concept

--- umic200/umic-temperature.js
+++ umic200/umic-temperature.js
@@ -1,8 +1,26 @@
 /*jshint esversion: 6 */ 
 
-const umic = require ('/home/umic/.node-red/node_modules/umic/build/Release/umic.node');
+//------------------------------------------------------------------------------------------------------
+//Try to load the umic library
+//
+let umic = '';
+//require('/usr/local/lib/node_modules/umic');
+try
+{
+    umic = require('/usr/local/lib/node_modules/umic2');
+} 
+catch (error)
+{
+    umic = require('./libs/umic_dummy');
+    console.log('Using umic dummy libary');
+}
 
+
+
+/*
 module.exports = function(RED) {
+	RED.log.info('node-red-contrib-umic version: ' + pkg.version);
+	
     function UmicGetSystemTemperature(config) {
         RED.nodes.createNode(this,config);
         
@@ -15,8 +33,50 @@ module.exports = function(RED) {
             msg.payload=result;
             node.send(msg);
         });
-        
-    }
-    RED.nodes.registerType("getSystemTemperature",UmicGetSystemTemperature);
-};
 
+    }
+    
+    RED.nodes.registerType("temp",UmicGetSystemTemperature);
+};
+*/
+
+module.exports = function (RED) {
+	
+    class UmicTempNode {
+        constructor(config) {
+            RED.nodes.createNode(this, config);
+            this.pin=config.pin;
+            this.outputs=config.outputs;
+            
+            this.on('input', this.input);
+            this.on('close', this.destructor);
+            
+            setInterval(this.trigger, 1200);
+        }
+        
+        destructor(done){
+        	done();
+        	
+        }
+        
+        output(msg) {
+        	let result = umic.info_get_system_temperature();
+            msg.payload=result;
+        	return msg.payload;
+        }
+        
+        input(msg) {
+        	RED.log.info('bingo ');
+        	let result = umic.info_get_system_temperature();
+            msg.payload=result;
+        }
+        
+        trigger()
+        {
+        	// RED.log.info('bongo ');
+        	// var newMsg = { payload: msg.payload.length }; --> this does not work
+        }
+    }
+    
+    RED.nodes.registerType('temp', UmicTempNode);
+}
