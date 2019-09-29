8405ace4aa94d426eb18bf4067bc33f29d0e8337
Uwe Koppe <koppe@microcontrol.net>
2019-09-29 19:27:59 +0200

    Initial dummy library

--- umic200/libs/umic200.js
+++ umic200/libs/umic200.js
@@ -1,11 +1,55 @@
+//------------------------------------------------------------------------------------------------------
+// This is a dummy library if the nodes are exectued on hardware other than the uMIC.200 controller
+//
 
-class uMIC200 
+const pinMax = 8;
+
+function umic_dio_get_input_pin(pinNumber)
 {
-    info_get_system_temperature()
+    let result = FALSE;
+    if (pinNumber > pinMax)
     {
-       return 260;
+        result = TRUE;
     }
+}
+
+function umic_dio_init()
+{
+
+}
+
+function umic_dio_set_direction_pin()
+{
+
+}
+
+function umic_dio_set_output_pin()
+{
     
 }
 
-module.exports = uMIC200;
+function umic_info_get_library_version()
+{
+    
+}
+
+function umic_info_get_serial_cpu()
+{
+   
+}
+
+function umic_info_get_system_temperature()
+{   
+    return 260;
+}
+    
+
+module.exports = {
+    dio_get_input_pin           : umic_dio_get_input_pin,
+    dio_init                    : umic_dio_init,
+    dio_set_direction_pin       : umic_dio_set_direction_pin,
+    dio_set_output_pin          : umic_dio_set_output_pin,
+    info_get_library_version    : umic_info_get_library_version,
+    info_get_serial_cpu         : umic_info_get_serial_cpu,
+    info_get_system_temperature : umic_info_get_system_temperature
+}
