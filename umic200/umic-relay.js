6e531dd9b9fa546f51378feef4db82b0fd523b5e
Uwe Koppe <koppe@microcontrol.net>
2019-10-01 16:31:15 +0200

    Update relay control

--- umic200/umic-relay.js
+++ umic200/umic-relay.js
@@ -1,41 +1,72 @@
 /*jshint esversion: 6 */ 
 
+"use strict";
+
 //------------------------------------------------------------------------------------------------------
 // Try to load the umic library
 //
 let umic = '';
-let info = '';
 try
 {
-  umic = require('/usr/local/lib/node_modules/umic');
+    umic = require('/usr/local/lib/umic.node');
 } 
 catch (error)
 {
   umic = require('./libs/umic200');
 }
 
+
+
 module.exports = function(RED) {
-    function UmicRelay(config) {
-        RED.nodes.createNode(this,config);
+    
+    //---------------------------------------------------------------------------------------------
+    // Definition of class 'UmicRelay'
+    //
+    class UmicRelay {
         
-        var node = this;
-        this.setTo=config.setTo;
+        //------------------------------------------------------------------------------------
+        // Constructor
+        //
+        constructor(config) 
+        {
+            RED.nodes.createNode(this, config);
+            
+            this.outputNumber = config.outputNumber;
+            
+            this.on('input', this.input);
+            this.on('close', this.close);
+            
+        }
+
         
-        node.on('input', function(msg) {
-        	
-        		if(this.setTo === "ON"){
-        			umic.relay_on();
-        		}
-        		
-        		if(this.setTo === "OFF"){
-        			umic.relay_off();
-        		}
-        		
-        		msg.payload='done';
-            node.send(msg);
-        });
+        //------------------------------------------------------------------------------------
+        // This method is called when the node is being stopped, e.g. a new flow
+        // configuration is deployed
+        //
+        close() 
+        {
+            
+        }
+        
+        //------------------------------------------------------------------------------------
+        // Set the state of the relay
+        //
+        input(msg) 
+        {
+            if (msg.payload === false)
+            {
+                umic.relay_off();
+            }
+            
+            if (msg.payload === true)
+            {
+                umic.relay_on();
+            }
+            
+        }
         
     }
+    
     RED.nodes.registerType("umic-relay", UmicRelay);
 };
 
